ca156b7abc9022f428329c0e97f1024c4c7f24c5
Eric Smith <ersmith@totalspectrum.ca>
2012-01-18 23:00:03 -0400

    allow difference in spacing (notably CR+LF, makes testing spin2cpp.exe easier

--- Test/runtests.sh
+++ Test/runtests.sh
@@ -7,7 +7,7 @@ for i in test*.spin
 do
   j=`basename $i .spin`
   $PROG $i
-  if  diff -u Expect/$j.h $j.h && diff -u Expect/$j.cpp $j.cpp
+  if  diff -ub Expect/$j.h $j.h && diff -ub Expect/$j.cpp $j.cpp
   then
       rm -f $j.h $j.cpp
       echo $j passed
