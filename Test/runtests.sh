b3e1ed45deb344c04247cda3c962e31bdbdaba5d
Eric Smith <ersmith@totalspectrum.ca>
2012-07-12 15:29:22 -0300

    fixed exec test

--- Test/runtests.sh
+++ Test/runtests.sh
@@ -32,14 +32,15 @@ do
   $CC -Os -u __serial_exit -o $j.elf $j.cpp FullDuplexSerial.cpp
   rm -f $j.out
   propeller-load $j.elf -r -t -q > $j.out
-  if diff -ub Expect/$j.txt $j.out
+  tail --lines=+6 $j.out >$j.txt
+  if diff -ub Expect/$j.txt $j.txt
   then
     echo $j passed
   else
     echo $j failed
     endmsg="TEST FAILURES"
   fi
-  rm -f $j.out $j.elf $j.cpp $j.h
+  rm -f $j.out $j.txt $j.elf $j.cpp $j.h
 done
 
 echo $endmsg
