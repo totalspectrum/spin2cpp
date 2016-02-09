6d2b453bf287c8a0c648ab80ade9b19ebff0a59b
Eric Smith <ersmith@totalspectrum.ca>
2016-02-08 21:44:58 -0400

    started work on spin2pasm

---
+++ ir.h
@@ -0,0 +1,73 @@
+/*
+ * Spin to Pasm converter
+ * Copyright 2016 Total Spectrum Software Inc.
+ * Intermediate representation definitions
+ */
+
+#ifndef SPIN_IR_H
+#define SPIN_IR_H
+
+typedef struct IR IR;
+typedef struct Register Register;
+
+enum IROpcode {
+    OPC_LABEL,
+    OPC_BYTE,
+    OPC_WORD,
+    OPC_LONG,
+
+    /* various instructions */
+    OPC_ADD,
+    OPC_SUB,
+    OPC_AND,
+    OPC_ANDN,
+    OPC_OR,
+    OPC_XOR,
+    OPC_SHL,
+    OPC_SHR,
+    OPC_SAR,
+    OPC_ROL,
+    OPC_ROR,
+    OPC_CMP,
+    OPC_NOT,
+    OPC_NEG,
+    OPC_CALL,
+    OPC_RET,
+    OPC_DJNZ,
+    OPC_JMP,
+};
+
+/* condition for conditional execution */
+enum IRCond {
+    COND_TRUE,
+    COND_FALSE,
+    COND_LT,
+    COND_GE,
+    COND_EQ,
+    COND_NE,
+    COND_LE,
+    COND_GT
+};
+
+struct IR {
+    enum IROpcode opc;
+    enum IRCond cond;
+    Register *src;
+    Register *dst;
+        
+    IR *prev;
+    IR *next;
+};
+
+enum Regkind {
+    REG_IMM,  // for an immediate value
+    REG_HW,   // for a hardware register
+    REG_REG,  // for a regular register
+};
+
+struct Register {
+    enum Regkind kind;
+    const char *name;
+};
+
+#endif
