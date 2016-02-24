2afa75706a4c9d81c399442ad6b37ec1fc29835e
Eric Smith <ersmith@totalspectrum.ca>
2016-02-24 08:05:43 -0400

    added test for IF/THEN/ELSE expression

---
+++ Test/Expect/test126.h
@@ -0,0 +1,12 @@
+#ifndef test126_Class_Defined__
+#define test126_Class_Defined__
+
+#include <stdint.h>
+
+class test126 {
+public:
+  static int32_t	Select(int32_t X, int32_t Y, int32_t Z);
+private:
+};
+
+#endif
