729215ebf0a9007cd95e6d946a408fd46204926e
Eric Smith <ersmith@totalspectrum.ca>
2018-12-12 06:05:55 -0400

    added a utility function for handling prefixes

---
+++ util/strdupcat.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include "util.h"
+
+char *strdupcat(char *a, char *b)
+{
+    size_t len = strlen(a) + strlen(b);
+    char *c = malloc(len+1);
+    strcpy(c, a);
+    strcat(c, b);
+    return c;
+}
