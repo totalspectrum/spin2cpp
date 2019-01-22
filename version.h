af71c62173c3d1aeb882b927496a5edb921a797e
Eric Smith <ersmith@totalspectrum.ca>
2019-01-21 22:26:57 -0400

    bump version

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 9
-#define VERSION_REV   15
-//#define BETA "-beta"
+#define VERSION_REV   16
+#define BETA "-beta"
 
 extern char version_string[];
 
