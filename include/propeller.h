6c3df43103f2e50780b0811e3bb7afe4fb6a0e00
Eric Smith <ersmith@totalspectrum.ca>
2019-02-02 22:22:18 -0400

    add cogstart

--- include/propeller.h
+++ include/propeller.h
@@ -1,3 +1,11 @@
+/**
+ * @file include/propeller.h
+ * @brief Provides Propeller specific functions.
+ *
+ * Copyright (c) 2011-2013 by Parallax, Inc.
+ * Copyright (c) 2019 Total Spectrum Software Inc.
+ * MIT Licensed
+ */
 #ifndef _PROPELLER_H_
 #define _PROPELLER_H_
 
@@ -16,5 +24,27 @@
 #endif
 
 #define CLKFREQ _clkfreq
+
+/**
+ * @brief Start a new propeller LMM function/thread in another COG.
+ *
+ * @details This function starts a new LMM VM kernel in a new COG with
+ * func as the start function. The stack size must be big enough to hold
+ * the struct _thread_state_t, the initial stack frame, and other stack
+ * frames used by called functions.
+ *
+ * @details This function can be used instead of _start_cog_thread.
+ *
+ * @param func LMM start function
+ * @param par Value of par parameter usually an address
+ * @param stack Address of user defined stack space.
+ * @param stacksize Size of user defined stack space.
+ * @returns COG ID allocated by the function or -1 on failure.
+ */
+#ifdef __FLEXC__
+#define cogstart(func, par, stack, stacksize) __builtin_cogstart(func(par), stack)
+#else
+int cogstart(void (*func)(void *), void *par, void *stack, size_t stacksize);
+#endif
 
 #endif
