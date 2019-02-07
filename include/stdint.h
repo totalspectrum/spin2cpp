74545cd0238f3b17c4be2f483e145c82bf1f50ed
Eric Smith <ersmith@totalspectrum.ca>
2019-02-07 07:42:54 -0400

    added int64_t definition

--- include/stdint.h
+++ include/stdint.h
@@ -10,6 +10,9 @@ typedef unsigned short uint16_t;
 typedef signed long   int32_t;
 typedef unsigned long uint32_t;
 
+typedef long long int16_t;
+typedef unsigned long long uint64_t;
+
 typedef unsigned long uintptr_t;
 typedef long intptr_t;
 
