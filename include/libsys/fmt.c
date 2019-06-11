8ab87ea781a370c2913307407a265b397487add8
Eric Smith <ersmith@totalspectrum.ca>
2019-06-11 19:17:53 -0300

    create stand-alone fmt.h

--- include/libsys/fmt.c
+++ include/libsys/fmt.c
@@ -14,61 +14,7 @@
 
 #define alloca(x) __builtin_alloca(x)
 
-//
-// string formatting functions
-//
-typedef int (*putfunc)(int c);
-#define CALL(fn, c) (*fn)(c)
-
-#ifdef SMALL_INT
-#define UITYPE uint32_t
-#define ITYPE  int32_t
-#define FTYPE float
-#else
-#define UITYPE uint64_t
-#define ITYPE  int64_t
-#define FTYPE double
-#endif
-#define BITCOUNT (8*sizeof(UITYPE))
-
-//
-// flags:
-//  8 bits maximum width
-//  8 bits minimum width
-//  6 bits precision
-//  2 bits padding character (0=none, 1=space, 2='0', 3=reserved)
-//  2 bits justification (0,1=left, 2=right, 3=center)
-//  2 bits sign character (0=none, 1='+', 2=' ', 3=unsigned)
-//  1 bit alt (for alternate form)
-//  1 bit for upper case
-//
-#define MAXWIDTH_BIT (0)
-#define WIDTH_MASK   0xff
-#define MINWIDTH_BIT (8)
-#define PREC_BIT     (16)
-#define PREC_MASK    0x1f
-#define JUSTIFY_BIT  (22)
-#define JUSTIFY_MASK 0x3
-#define PADCHAR_BIT  (24)
-#define PADCHAR_MASK 0x3
-#define SIGNCHAR_BIT (26)
-#define SIGNCHAR_MASK 0x3
-#define ALTFMT_BIT   (28)
-#define UPCASE_BIT   (29)
-
-#define JUSTIFY_LEFT  1
-#define JUSTIFY_RIGHT 2
-#define JUSTIFY_CENTER 3
-
-#define PADCHAR_NONE  0
-#define PADCHAR_SPACE 1
-#define PADCHAR_ZERO  2
-
-#define SIGNCHAR_NONE 0
-#define SIGNCHAR_PLUS 1
-#define SIGNCHAR_SPACE 2
-#define SIGNCHAR_UNSIGNED 3
-#define SIGNCHAR_MINUS 4
+#include "sys/fmt.h"
 
 #define DEFAULT_PREC 4
 #define DEFAULT_FLOAT_FMT ((1<<ALTFMT_BIT)|(1<<UPCASE_BIT))
