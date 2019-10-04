317bfbe5f213b2120d72e916d9b7d08bd036dedc
Eric Smith <ersmith@totalspectrum.ca>
2019-10-04 17:03:49 -0300

    change version number to 4.0.0

--- version.h
+++ version.h
@@ -4,9 +4,9 @@
 #define str__(x) #x
 #define str_(x) str__(x)
 
-#define VERSION_MAJOR 3
-#define VERSION_MINOR 9
-#define VERSION_REV   34
+#define VERSION_MAJOR 4
+#define VERSION_MINOR 0
+#define VERSION_REV   0
 #define BETA "-beta"
 
 #define VERSIONSTR version_string
