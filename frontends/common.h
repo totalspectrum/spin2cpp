f41612288ee214df4b514305cdd3c272a94917f6
Eric Smith <ersmith@totalspectrum.ca>
2019-11-07 16:46:04 -0400

    made version 2b the default for Prop2 compilation

--- frontends/common.h
+++ frontends/common.h
@@ -86,7 +86,7 @@ struct lexstream {
 extern int gl_p2;      /* set for P2 output */
 # define P2_REV_A 1
 # define P2_REV_B 2
-#define DEFAULT_P2_VERSION P2_REV_A
+#define DEFAULT_P2_VERSION P2_REV_B
 
 extern int gl_output;  /* type of output to produce */
 extern int gl_outputflags; /* modifiers (e.g. LMM or COG code */
