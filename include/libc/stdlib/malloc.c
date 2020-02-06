1e8a134b621379260d7d1e152bc08406d5491709
Eric Smith <ersmith@totalspectrum.ca>
2020-02-06 14:51:55 -0400

    fixed missing realloc definition

--- include/libc/stdlib/malloc.c
+++ include/libc/stdlib/malloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 void *malloc(size_t size) {
     return _gc_alloc(size);
@@ -10,4 +11,22 @@ void *calloc(size_t n, size_t size) {
 
 void free(void *ptr) {
     return _gc_free(ptr);
+}
+
+void *realloc(void *ptr, size_t size)
+{
+    void *newptr;
+    if (ptr == 0) {
+        return malloc(size);
+    }
+    if (size == 0) {
+        free(ptr);
+        return 0;
+    }
+    newptr = malloc(size);
+    if (newptr) {
+        memcpy(newptr, ptr, size);
+        free(ptr);
+    }
+    return newptr;
 }
