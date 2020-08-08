29911d226a213f304606fec161b009a18341c1fe
Eric Smith <ersmith@totalspectrum.ca>
2020-08-08 11:02:15 -0300

    started on 4.3 development

--- version.h
+++ version.h
@@ -5,8 +5,8 @@
 #define str_(x) str__(x)
 
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 2
-#define VERSION_REV   8
+#define VERSION_MINOR 3
+#define VERSION_REV   0
 #define BETA "-beta"
 
 #define VERSIONSTR version_string
