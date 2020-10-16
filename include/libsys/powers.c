c35ca7b6842359119823f2de0602840303e29bb2
Eric Smith <ersmith@totalspectrum.ca>
2020-10-16 10:36:23 -0300

    improved P1 implementation of exp and log by interpolating table entries

--- include/libsys/powers.c
+++ include/libsys/powers.c
@@ -49,11 +49,16 @@ unsigned __builtin_qlog(unsigned num)
 {
     unsigned exp;
     unsigned r;
+    unsigned r2;
+    unsigned frac;
     exp = __builtin_clz(num);
     num = num << exp;  // left justify
-    num = num >> (30-11);
-    num = num & 0xffe;
+    // we will look up based on 11 bits, so the bottom 16 bits are the fraction
+    frac = num & 0xffff;
+    num = (num >> (30-11)) & 0xffe;  // 30 because we will multiply by 2 for word access
     r = *((unsigned short *)(0xC000 + num));
+    r2 = *((unsigned short *)(0xC002 + num));
+    r = r + (((r2-r)*frac) >> 16);
     exp = exp ^ 0x1f;
     r = r | (exp << 16);
 
@@ -66,13 +71,20 @@ unsigned __builtin_qlog(unsigned num)
 unsigned __builtin_qexp(unsigned orig_num)
 {
     unsigned exp;
-    unsigned r;
+    unsigned r, r2;
+    unsigned frac;
     unsigned num = orig_num;
     
     exp = (num >> 27);
+    // the  original value is 5.27
+    // we will look up based on 11 bits, so the bottom 16 bits are the fraction
+    frac = num & 0xffff;
     num = (num >> (26-11)) & 0x0ffe;
-    //printf("...num=0x%08x lookup=%08x exp=%d\n", orig_num, num, exp);
+    //__builtin_printf("...num=0x%08x lookup=%08x exp=%d\n", orig_num, num, exp);
     r = *((unsigned short *)(0xD000 + num));
+    r2 = *((unsigned short *)(0xD002 + num));
+    //__builtin_printf("...r1=0x%08x r2=%08x frac=%04x\n", r, r2, frac);
+    r = r + ((frac * (r2-r)) >> 16);
     r = r << 15; // shift into 30..15
     r |= 0x80000000;
     //printf("...r=0x%08x\n", r);
