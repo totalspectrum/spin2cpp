100e8b6a02cd72095aa1d7eaab3155a4c82db449
Eric Smith <ersmith@totalspectrum.ca>
2021-04-07 22:11:30 -0300

    added partial float.h file

---
+++ include/float.h
@@ -0,0 +1,20 @@
+#ifndef _FLOAT_H
+#define _FLOAT_H
+
+#include <compiler.h>
+
+#define FLT_ROUNDS (1)  /* round to nearest */
+#define FLT_RADIX  (2)
+
+#define FLT_MANT_DIG (24)
+#define FLT_DIG      (7)
+
+#define FLT_EPSILON  (1.192092891e-07)
+#define FLT_MIN_EXP  (-63)
+#define FLT_MIN      (5.421919547e-20)
+#define FLT_MIN_10_EXP (-19)
+#define FLT_MAX      (9.2233715e18)
+#define FLT_MAX_10_EXP (18)
+
+
+#endif
