912078feae2066bcbd15a1e626a85310cd7f1c83
Eric Smith <ersmith@totalspectrum.ca>
2021-06-25 17:34:57 -0300

    re-factored a bit

---
+++ backends/becommon.c
@@ -0,0 +1,75 @@
+/*
+ * Spin to C/C++ translator
+ * Copyright 2011-2021 Total Spectrum Software Inc.
+ * 
+ * +--------------------------------------------------------------------
+ * ¦  TERMS OF USE: MIT License
+ * +--------------------------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * +--------------------------------------------------------------------
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "common.h"
+#include "becommon.h"
+
+static AST *make_methodptr;
+
+AST *
+BuildMethodPointer(AST *ast)
+{
+    Symbol *sym;
+    AST *objast;
+    AST *funcaddr;
+    AST *result;
+    Function *func;
+    
+    sym = FindCalledFuncSymbol(ast, &objast, 0);
+    if (!sym || sym->kind != SYM_FUNCTION) {
+        if (sym) {
+            ERROR(ast, "%s is not a function", sym->user_name);
+        } else {
+            ERROR(ast, "Internal error, unable to find function address");
+        }
+        return ast;
+    }
+    func = (Function *)sym->val;
+    if (func->is_static) {
+        objast = AstInteger(0);
+    } else if (objast == NULL) {
+        objast = NewAST(AST_SELF, NULL, NULL);
+    } else {
+        objast = NewAST(AST_ADDROF, objast, NULL);
+    }
+    func->used_as_ptr = 1;
+    if (func->callSites == 0) {
+        MarkUsed(func, "func pointer");
+    }
+    // save off the current @ node
+    funcaddr = NewAST(AST_ADDROF, ast->left, ast->right);
+    // create a call
+    if (!make_methodptr) {
+        make_methodptr = getBasicPrimitive("_make_methodptr");
+    }
+    result = MakeOperatorCall(make_methodptr, objast, funcaddr, NULL);
+    return result;
+}
---
+++ backends/becommon.h
@@ -0,0 +1,10 @@
+#ifndef BACKEND_COMMON_H
+#define BACKEND_COMMON_H
+
+#include "ast.h"
+#include "symbol.h"
+#include "expr.h"
+
+AST *BuildMethodPointer(AST *ast);
+
+#endif /* BACKEND_COMMON_H */
