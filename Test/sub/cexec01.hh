09a4bc36f59ffd9f98740d7d5a6a078a8e35057c
Eric Smith <ersmith@totalspectrum.ca>
2021-06-27 19:48:27 -0300

    add printhex to cexec01.hh (for debugging purposes)

--- Test/sub/cexec01.hh
+++ Test/sub/cexec01.hh
@@ -6,6 +6,7 @@
 #define println(x) printf(x "\n")
 #define print(x) printf("%s", x)
 #define printdec(x) printf("%d", x)
+#define printhex(x) printf("%x", x)
 #else
 # ifdef __P2__
 extern struct __using("spin/SmartSerial.spin") fds;
@@ -16,6 +17,7 @@ extern struct __using("spin/FullDuplexSerial.spin") fds;
 #define print(x) fds.str(x)
 #define putchar(c) fds.tx(c)
 #define printdec(x) fds.dec(x)
+#define printhex(x) fds.hex(x,8)
 #endif
 
 
