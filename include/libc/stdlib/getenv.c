3a8c50d2d29188ed6878003301a755314f956257
Eric Smith <ersmith@totalspectrum.ca>
2021-09-10 12:57:49 -0300

    pull putenv into its own file to improve -O0 linking

--- include/libc/stdlib/getenv.c
+++ include/libc/stdlib/getenv.c
@@ -25,34 +25,3 @@ getenv(const char *tag)
 	}
 	return 0;
 }
-
-int
-putenv(const char *strng)
-{
-    size_t idx;
-    const char **var;
-    const char *name;
-    size_t len = strlen(strng);
-    
-    if (!_environ_ptr) {
-        name = 0;
-        var = 0;
-    } else {
-        for (var = _environ_ptr; (name = *var) != 0; var++) {
-            if (!strncmp(name, strng, len) && name[len] == '=')
-                break;
-        }
-    }
-    if (name) {
-        // replace old value
-        *var = strng;
-        return 0;
-    }
-    idx = (var - _environ_ptr);
-    var = realloc( _environ_ptr, (idx+2) * sizeof(char *) );
-    if (!var) {
-        return _seterror(ENOMEM);
-    }
-    var[idx] = strng;
-    var[idx+1] = 0;
-}
