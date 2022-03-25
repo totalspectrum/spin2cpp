f9641e45d722891468c922ac1e232de5b56e3f97
Ada Gottensträter <nunuu@gmx.net>
2022-03-25 22:51:14 +0100

    Allow memory optimization over offset sequences

--- backends/asm/optimize_ir.c
+++ backends/asm/optimize_ir.c
@@ -3270,6 +3270,7 @@ static IR* FindNextUse(IR *ir, Operand *dst)
 static IR* FindNextRead(IR *irorig, Operand *dest, Operand *src)
 {
     IR *ir;
+    int32_t offset = 0;
     for ( ir = irorig->next; ir; ir = ir->next) {
         if (IsDummy(ir)) continue;
         if (ir->opc == OPC_LABEL) {
@@ -3281,10 +3282,13 @@ static IR* FindNextRead(IR *irorig, Operand *dest, Operand *src)
         if (ir->cond != irorig->cond) {
             return NULL;
         }
-        if (ir->src == src && (ir->opc == OPC_RDLONG||ir->opc == OPC_RDWORD||ir->opc == OPC_RDBYTE)) {
+        if (ir->src == src && offset == 0 && (ir->opc == OPC_RDLONG||ir->opc == OPC_RDWORD||ir->opc == OPC_RDBYTE)) {
             return ir;
         }
-        if (InstrModifies(ir, dest) || InstrModifies(ir, src)) {
+        if ((ir->opc == OPC_ADD || ir->opc == OPC_SUB) && ir->dst == src && ir->src && ir->src->kind == IMM_INT) {
+            if (ir->opc == OPC_ADD) offset += ir->src->val;
+            else                    offset -= ir->src->val;
+        } else if (InstrModifies(ir, dest) || InstrModifies(ir, src)) {
             return NULL;
         }
         if (IsWrite(ir)) {
