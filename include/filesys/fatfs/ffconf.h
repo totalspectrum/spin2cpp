f1bea9f54088afc278d7c0abadecdcbfeef6d612
Eric Smith <ersmith@totalspectrum.ca>
2022-04-22 08:28:12 -0300

    change default code page to Latin 1

--- include/filesys/fatfs/ffconf.h
+++ include/filesys/fatfs/ffconf.h
@@ -84,7 +84,11 @@
 / Locale and Namespace Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_CODE_PAGE	932
+#ifndef FF_CODE_PAGE
+#define FF_CODE_PAGE    850
+//#define FF_CODE_PAGE	932
+#endif
+
 /* This option specifies the OEM code page to be used on the target system.
 /  Incorrect code page setting can cause a file open failure.
 /
