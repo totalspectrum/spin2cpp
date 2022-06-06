806a2bdc8dc81d03a08755c7a490c1a65be852f4
Eric Smith <ersmith@totalspectrum.ca>
2022-06-06 16:38:40 -0300

    sys_errlist can be static

--- include/libc/string/strerror.c
+++ include/libc/string/strerror.c
@@ -1,7 +1,7 @@
 #include <errno.h>
 #include <string.h>
 
-char *_sys_errlist[] = {
+static const char * const _sys_errlist[] = {
   "OK",
   "Numerical argument out of domain",
   "Result not representable",
