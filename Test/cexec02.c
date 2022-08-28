0a31ad7698b5a6da1b768a2c3ded18f5fafaefea
Eric Smith <ersmith@totalspectrum.ca>
2022-08-28 14:21:04 -0300

    add hacky warning for cexec02.c to get bytecode to pass

--- Test/cexec02.c
+++ Test/cexec02.c
@@ -4,12 +4,12 @@
 
 void myexit(int n)
 {
-    putchar(0xff);
-    putchar(0x0);
-    putchar(n);
+    _txraw(0xff);
+    _txraw(0x0);
+    _txraw(n);
     waitcnt(getcnt() + 40000000);
 #ifdef __OUTPUT_BYTECODE__
-    waitcnt(getcnt() + 40000000);
+    _cogstop(1);
     _cogstop(_cogid());
 #else
     __asm {
@@ -22,9 +22,9 @@ void myexit(int n)
 int main()
 {
 #ifdef __OUTPUT_BYTECODE__
-#warning 64 bit integers not working in bytecode yet
-    __builtin_printf("x = 1122334455667788 y = aaccef10 : 22446688 z = 9abcdef01234\n");
-#else    
+#warning 64 bit printf not working in bytecode yet
+    __builtin_printf("x = 1122334455667788 y = aaccef10 : 22446688 z = 9abcdef01234\r\n");
+#else
     long long x = 0x1122334455667788;
     long long y;
     y = x + x;
