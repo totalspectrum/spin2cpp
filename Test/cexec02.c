bdae6c0dc7c0b21ddd6e1e6f619ddd97aed4e5b0
Eric Smith <ersmith@totalspectrum.ca>
2022-08-30 11:56:30 -0300

    updated cexec02 bytecode test

--- Test/cexec02.c
+++ Test/cexec02.c
@@ -21,14 +21,9 @@ void myexit(int n)
 
 int main()
 {
-#ifdef __OUTPUT_BYTECODE__
-#warning 64 bit printf not working in bytecode yet
-    __builtin_printf("x = 1122334455667788 y = aaccef10 : 22446688 z = 9abcdef01234\r\n");
-#else
     long long x = 0x1122334455667788;
     long long y;
     y = x + x;
     printf("x = %llx y = %x : %x z = %llx\n", x, y, 0x9abcdef01234LL, 1);
-#endif
     myexit(0);
 }
