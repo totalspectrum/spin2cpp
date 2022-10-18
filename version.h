183cd6c67ff8a417c4d3f0021514af9adc78f4e0
Eric Smith <ersmith@totalspectrum.ca>
2022-10-18 08:56:29 -0300

    updated version and Changelog

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 9
-#define VERSION_REV   18
-//#define BETA "-beta"
+#define VERSION_REV   19
+#define BETA "-beta"
 
 #define VERSIONSTR version_string
 
