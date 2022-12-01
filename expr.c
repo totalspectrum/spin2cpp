96c2ad406ac30fa23379cc13850dc8bd27eeba0d
Eric Smith <ersmith@totalspectrum.ca>
2022-12-01 14:24:01 -0400

    provide alternate definitions for popcount and clz

--- expr.c
+++ expr.c
@@ -15,6 +15,28 @@
 #include <stdlib.h>
 #include <limits.h>
 
+#if defined(__GNUC__) || defined(__clang__)
+#define clz32(x) __builtin_clz((uint32_t)(x))
+#define popcount(x) __builtin_popcount(x)
+#else
+static int clz32(uint32_t x) {
+    unsigned r = 0;
+    for (r = 0; r < 32; r++) {
+        if ( ((int32_t)x) < 0) break;
+        x = x<<1;
+    }
+    return r;
+}
+
+static int popcount(uint64_t x) {
+    int n = 0;
+    for (unsigned i = 0; i < 64; i++) {
+        if (x & (1ULL<<i)) n++;
+    }
+    return n;
+}
+#endif
+
 /* get the internal identifier name from an identifier */
 const char *
 GetIdentifierName(AST *expr)
@@ -1291,9 +1313,9 @@ EvalIntOperator(int op, ExprInt lval, ExprInt rval, int *valid)
     case K_DECODE:
         return 1L << (rval&0x1f);
     case K_ENCODE:
-        return 32 - __builtin_clz(rval);
+        return 32 - clz32(rval);
     case K_ENCODE2:
-        return (rval == 0) ? 0 : 31-__builtin_clz(rval);
+        return (rval == 0) ? 0 : 31-clz32(rval);
     case K_LIMITMIN:
         return (lval < rval) ? rval : lval;
     case K_LIMITMAX:
@@ -1325,7 +1347,7 @@ EvalIntOperator(int op, ExprInt lval, ExprInt rval, int *valid)
         }
         return 0;
     case K_ONES_COUNT:
-        return __builtin_popcount(rval);
+        return popcount(rval);
     case K_QLOG:
         {
             double e = log2((double)(uint32_t)rval);
