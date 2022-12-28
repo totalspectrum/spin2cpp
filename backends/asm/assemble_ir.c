527d0bbd85d623499f6412b4465fb30530b7d9b3
Eric Smith <ersmith@totalspectrum.ca>
2022-12-28 10:29:36 -0400

    tweak output a bit

--- backends/asm/assemble_ir.c
+++ backends/asm/assemble_ir.c
@@ -239,7 +239,12 @@ doPrintOperand(struct flexbuf *fb, Operand *reg, int useimm, enum OperandEffect 
         }
         flexbuf_addstr(fb, RemappedName(regname));
         if ( (reg->kind == REG_HW || reg->kind == IMM_COG_LABEL) && reg->val != 0) {
-            flexbuf_printf(fb, " + %d", reg->val);
+            int32_t off = reg->val;
+            if (off < 0) {
+                flexbuf_printf(fb, " - %d", -off);
+            } else {
+                flexbuf_printf(fb, " + %d", off);
+            }
         }
         if (effect == OPEFFECT_POSTINC) {
             flexbuf_printf(fb, "++");
