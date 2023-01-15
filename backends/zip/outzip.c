3a829b96aa3c95e0c41cc6fa1988bc952218f9ba
Eric Smith <ersmith@totalspectrum.ca>
2023-01-15 01:00:46 -0400

    do not put absolute paths in zip files

--- backends/zip/outzip.c
+++ backends/zip/outzip.c
@@ -14,7 +14,8 @@ OutputZipFile(const char *zipname)
     SourceFile *sf;
     int i;
     struct zip_t *zip;
-
+    const char *zipEntryName;
+    
     zip = zip_open(zipname, ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
     if (!zip) {
         perror(zipname);
@@ -24,7 +25,16 @@ OutputZipFile(const char *zipname)
     for (i = 0; i < numSourceFiles; i++) {
         sf = sourceData + i;
         if (sf->isSysFile) continue;
-        zip_entry_open(zip, sf->shortName);
+        zipEntryName = sf->shortName;
+#ifdef WIN32
+        if (zipEntryName[0] && zipEntryName[1] == ':') {
+            zipEntryName += 2;
+        }
+        while (zipEntryName[0] == '\\') zipEntryName++;
+#endif        
+        while (zipEntryName[0] == '/') zipEntryName++;
+        
+        zip_entry_open(zip, zipEntryName);
         if (zip_entry_fwrite(zip, sf->fullName) != 0) {
             ERROR(NULL, "Unable to write file %s to zip", sf->fullName);
             return;
