2c73f30add2f211198617bafd2e13c0a3af8c67f
Eric Smith <ersmith@totalspectrum.ca>
2023-01-18 17:43:50 -0400

    some fs9p optimizations

--- version.h
+++ version.h
@@ -6,8 +6,8 @@
 
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 9
-#define VERSION_REV   26
-//#define BETA "-beta"
+#define VERSION_REV   27
+#define BETA "-beta"
 
 #define VERSIONSTR version_string
 
