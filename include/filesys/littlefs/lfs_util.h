87b61da6d3b85df244011a40f116f3d9cd221ba9
Eric Smith <ersmith@totalspectrum.ca>
2023-03-11 19:11:04 -0400

    disable LFS warning/error printfs

--- include/filesys/littlefs/lfs_util.h
+++ include/filesys/littlefs/lfs_util.h
@@ -7,6 +7,16 @@
 #ifndef LFS_UTIL_H
 #define LFS_UTIL_H
 
+#ifdef __FLEXC__
+#define LFS_NO_MALLOC
+#ifndef _DEBUG
+# define LFS_NO_ASSERT
+# define LFS_NO_DEBUG
+# define LFS_NO_WARN
+# define LFS_NO_ERROR
+#endif
+#endif
+
 // Users can override lfs_util.h with their own configuration by defining
 // LFS_CONFIG as a header file to include (-DLFS_CONFIG=lfs_config.h).
 //
