0b9aedddadbebf6a501058bd2b5f195c0b2e9ea5
Eric Smith <ersmith@totalspectrum.ca>
2023-03-29 12:52:28 -0300

    update version

--- version.h
+++ version.h
@@ -5,9 +5,9 @@
 #define str_(x) str__(x)
 
 #define VERSION_MAJOR 6
-#define VERSION_MINOR 0
-#define VERSION_REV   5
-//#define BETA "-beta"
+#define VERSION_MINOR 1
+#define VERSION_REV   0
+#define BETA "-beta"
 
 #define VERSIONSTR version_string
 
