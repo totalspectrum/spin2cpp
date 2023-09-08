a88394d21e6b6cb38f79d1223b72f6c6f77a1196
Eric Smith <ersmith@totalspectrum.ca>
2023-09-08 20:32:24 -0300

    Added support for NeoYume style ARGv parameter passing (needs new loadp2)

--- include/libsys/c_startup.c
+++ include/libsys/c_startup.c
@@ -4,19 +4,33 @@
 #include <sys/ioctl.h>
 #include <stdlib.h>
 
-static const char *_argv[] = {
-    "program",
-    NULL
-};
+#define MAX_ARGC 32
+#define ARGV_MAGIC ('A' | ('R' << 8) | ('G'<<16) | ('v'<<24))
 
-#define _argc (sizeof(_argv) / sizeof(_argv[0]))
+#define START_ARGS ((char *)0xFC004)
+#define END_ARGS   ((char *)0xFCFFF)
+
+static const char *_argv[MAX_ARGC];
 
 void _c_startup()
 {
     int r;
+    int argc = 0;
+
     _setrxtxflags(TTY_FLAG_ECHO); // make sure TTY_FLAG_CRNL is off
     _waitms(20);  // brief pause
-    r = main(_argc, _argv);
+
+    _argv[argc++] = "program";
+    if ( ARGV_MAGIC == (*(long *)0xFC000) ) {
+        char *arg = START_ARGS;
+        while (arg < END_ARGS && *arg != 0 && argc < MAX_ARGC-1) {
+            _argv[argc++] = arg;
+            while (*arg != 0 && arg < END_ARGS) arg++;
+            arg++;
+        }
+    }
+    _argv[argc] = NULL;
+    r = main(argc, _argv);
     _waitms(20);  // brief pause
     _Exit(r);
 }
