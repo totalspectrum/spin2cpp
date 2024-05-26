8d687747695318024a9ae44ff70f293186c4e3dc
Eric Smith <ersmith@totalspectrum.ca>
2024-05-26 10:24:34 -0300

    better handling of names in .zip files

--- backends/zip/outzip.c
+++ backends/zip/outzip.c
@@ -8,6 +8,15 @@
 #include "cmdline.h"
 #include "zip.h"
 
+static const char *
+ShortZipName(SourceFile *sf, const char *root, size_t rootlen)
+{
+    if (!strncmp(sf->fullName, root, rootlen)) {
+        return sf->fullName + rootlen;
+    }
+    return strdupcat("library/", sf->shortName);
+}
+
 void
 OutputZipFile(const char *zipname)
 {
@@ -15,6 +24,12 @@ OutputZipFile(const char *zipname)
     int i;
     struct zip_t *zip;
     const char *zipEntryName;
+    char *rootname;
+    char *ptr, *lastslash;
+    size_t rootnamelen;
+    
+    if (numSourceFiles == 0)
+        return;
     
     zip = zip_open(zipname, ZIP_DEFAULT_COMPRESSION_LEVEL, 'w');
     if (!zip) {
@@ -22,10 +37,28 @@ OutputZipFile(const char *zipname)
         gl_errors++;
         return;
     }
+    ptr = rootname = strdup(sourceData[0].fullName);
+    lastslash = 0;
+    while (*ptr) {
+        if (*ptr == '/'
+#ifdef WIN32
+            || *ptr == '\\'
+#endif
+            )
+            lastslash = ptr;
+        ptr++;
+    }
+    if (lastslash) {
+        lastslash[1] = 0;
+    } else {
+        rootname = "";
+    }
+    rootnamelen = strlen(rootname);
+
     for (i = 0; i < numSourceFiles; i++) {
         sf = sourceData + i;
         if (sf->isSysFile) continue;
-        zipEntryName = sf->shortName;
+        zipEntryName = ShortZipName(sf, rootname, rootnamelen);
 #ifdef WIN32
         if (zipEntryName[0] && zipEntryName[1] == ':') {
             zipEntryName += 2;
