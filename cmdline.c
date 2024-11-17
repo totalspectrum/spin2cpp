cb2ee721fe0b789dd4d5b9f6d6cabef455b0b0b2
Eric Smith <ersmith@totalspectrum.ca>
2024-11-17 13:53:16 -0400

    Suppress some asm warnings when building compiler output

--- cmdline.c
+++ cmdline.c
@@ -427,7 +427,7 @@ int ProcessCommandLine(CmdLineOptions *cmd)
             }
             if (cmd->compile)  {
                 gl_caseSensitive = !compile_original;
-                gl_warn_flags &= ~WARN_ASM_FIRST_PASS; // already issued warnings
+                gl_warn_flags &= ~(WARN_ASM_FIRST_PASS|WARN_ASM_USAGE); // already issued warnings
                 gl_optimize_flags = 0; // no need to re-optimize
                 gl_cenv_flags = 0;     // already handled C startup code
                 CompileAsmToBinary(binname, asmname);
