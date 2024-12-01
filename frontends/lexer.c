0b6e6becd45e314ecab2f169db358ee224a4c41e
Eric Smith <ersmith@totalspectrum.ca>
2024-12-01 09:44:25 -0400

    bump max version accepted

--- frontends/lexer.c
+++ frontends/lexer.c
@@ -23,7 +23,7 @@
 
 #define VT '\013'
 
-#define MAX_PNUT_VERSION 45 /* maximum PNut version we understand */
+#define MAX_PNUT_VERSION 46 /* maximum PNut version we understand */
 
 int allow_type_names = 1;
 
